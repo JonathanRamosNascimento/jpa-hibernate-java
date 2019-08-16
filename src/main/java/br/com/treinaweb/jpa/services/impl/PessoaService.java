607182096d909268091b235add1e2e23fea947a9
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-15 22:21:51 -0300

    GetAll and Insert Method Creation

---
+++ src/main/java/br/com/treinaweb/jpa/services/impl/PessoaService.java
@@ -0,0 +1,69 @@
+package br.com.treinaweb.jpa.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.treinaweb.jpa.models.Pessoa;
+import br.com.treinaweb.jpa.services.interfaces.CrudService;
+import br.com.treinaweb.jpa.utils.JpaUtils;
+
+public class PessoaService implements CrudService<Pessoa, Integer> {
+
+	@Override
+	public List<Pessoa> all() {
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		EntityManager em =null;
+		try {
+			em = JpaUtils.getEntityManager();
+			pessoas = em.createQuery("from Pessoa", Pessoa.class).getResultList();
+			return pessoas;
+		} finally {
+			if(em != null) {
+				em.clear();
+			}
+		}
+	}
+
+	@Override
+	public Pessoa byId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pessoa insert(Pessoa entity) {
+		EntityManager em = null;
+		try {
+			em =JpaUtils.getEntityManager();
+			em.getTransaction().begin();
+			em.persist(entity);
+			em.getTransaction().commit();
+			return entity;
+		}finally {
+			if(em != null) {
+				em.close();
+			}
+		}
+	}
+
+	@Override
+	public Pessoa update(Pessoa entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Pessoa entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
